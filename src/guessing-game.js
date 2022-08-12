b26614cacc0c80bbdaef970679489992c3d24871
kornienkokostia <kornienkokostia@gmail.com>
2022-08-12 11:40:30 +0300

    final

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null
+        this.max = null
+        this.root = null
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.root = Math.ceil((this.max - this.min) / 2 + this.min);
+        return this.root
     }
 
     lower() {
-
+        this.max = this.root
     }
 
     greater() {
-
+        this.min = this.root
     }
 }
 
